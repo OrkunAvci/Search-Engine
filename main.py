0385a46a12515b04b85fa47849f48c74d8ad81f7
OrkunAvci <orkunavci28@gmail.com>
2021-11-30 21:52:56 +0300

    Proof of concept test

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import scrapy
+import string
+
+raw_text = """..."""
+
+clean_text = raw_text.strip(string.punctuation)
+words = clean_text.split(" ")
+
+dic = {}
+for key in words:
+	if key not in dic.keys() :
+		dic[key] = 0
+	dic[key] = dic[key] + 1
+
+print(dic)
