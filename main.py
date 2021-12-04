c14d2b8910628128fa4a871d50d427fa72d959a5
OrkunAvci <orkunavci28@gmail.com>
2021-12-04 22:08:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import textnanipulations as tm
+import json
 
 raw_text = """..."""
 first = tm.frequency_table(raw_text)
 
-raw_text_2 = """..."""
-second = tm.frequency_table(raw_text_2)
+with open("random.txt", "w") as write:
+	print(json.dumps(first))
+	write.write(json.dumps(first))
 
-print(first)
-print(second)
-print(tm.encapsulate(first))
-print(tm.encapsulate(second))
+with open("random.txt", "r") as read:
+	back = json.loads(read.read())
+	print(back)
