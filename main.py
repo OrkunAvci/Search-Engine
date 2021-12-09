80f1fd7085179296ddcd2f7a7d06b09d63c6f75c
OrkunAvci <orkunavci28@gmail.com>
2021-12-09 14:34:14 +0300

    Clean up

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import textnanipulations as tm
 import json
 
-raw_text = """..."""
-first = tm.frequency_table(raw_text)
-
-with open("random.txt", "w") as write:
-	print(json.dumps(first))
-	write.write(json.dumps(first))
-
-with open("random.txt", "r") as read:
-	back = json.loads(read.read())
-	print(back)
+# raw_text = """..."""
+# first = tm.frequency_table(raw_text)
+#
+# with open("random.txt", "w") as write:
+# 	print(json.dumps(first))
+# 	write.write(json.dumps(first))
+#
+# with open("random.txt", "r") as read:
+# 	back = json.loads(read.read())
+# 	print(back)
