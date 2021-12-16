4427e6349e5a65669a47d19bde781b9b8a24c6ae
OrkunAvci <orkunavci28@gmail.com>
2021-12-16 20:55:00 +0300

    Make reverse index

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import process_pages as pp
+import file_manager as fm
 import pprint
 
-data = pp.get_text_from_url("https://orkunavci.hashnode.dev/master-google-in-3-minutes")
+all_links = fm.get("_all_links")
+guide = fm.get("_guide")
 
-pprint.pprint(data)
+reverse_index = guide
+for key in reverse_index.keys():
+	reverse_index[key] = []
+
+for link in all_links:
+	ft = fm.get("ft_" + link)
+	for keyword,count in ft.items():
+		reverse_index[keyword].append({
+			"link": link,
+			"count": count,
+			"TF-IDF": 0
+		})
+
+pprint.pprint(reverse_index)    # BIG text, use with a terminal that can handle it
+
+fm.save("_reverse_index", reverse_index)
